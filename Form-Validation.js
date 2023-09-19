1500f3547f10801a29a978ea45785725f09b4833
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-19 21:33:06 +0100

    modified

--- Form-Validation.js
+++ Form-Validation.js
@@ -1,4 +1,25 @@
- 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUCKi6uKaSi1uruGc28x3jqWOoR9b2cBs",
+  authDomain: "form-fccde.firebaseapp.com",
+  projectId: "form-fccde",
+  storageBucket: "form-fccde.appspot.com",
+  messagingSenderId: "105154081246",
+  appId: "1:105154081246:web:cc4b55c88acf9e2dd5ad4f",
+  measurementId: "G-BDVDELV1EK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); 
+
            document.title='Form Validation'
            
            function validation() {
